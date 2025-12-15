802b7a5771ff507e2b2dfb2e80f62f8e3bdb2b45
2090741942 <2090741942@qq.com>
2025-12-15 09:22:28 +0000

    new data unzip

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-data_full = pd.read_csv("/workspace/xrd-gpt/jarvis_data/id_prop/id_prop_train.csv", encoding="utf-8")
-data_quarter = pd.read_csv("/workspace/xrd-gpt/jarvis_data/id_prop_quarter/id_prop_train_quarter.csv", encoding="utf-8")
+data_full = pd.read_csv("/workspace/xrd-gpt/JARVIS_90/id_prop_train.csv", encoding="utf-8")
+data_quarter = pd.read_csv("/workspace/xrd-gpt/JARVIS_90/id_prop_quarter/id_prop_train_quarter.csv", encoding="utf-8")
 
 print(data_full.info)
 print(data_quarter.info)
